9ce5628f1bcfdd592afa6a4839ea94b00e53ac01
Dima Tsurkan <d-link13@mail.ru>
2021-04-21 22:01:04 +0300

    extended load function to deliver an error and added test for it

--- EssentialFeed/EssentialFeed/Feed API /RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API /RemoteFeedLoader.swift
@@ -8,18 +8,25 @@
 import Foundation
 
 public protocol HTTPClient {
-  func get(url: URL)
+  func get(url: URL, completion: @escaping (Error) -> Void)
 }
 
-public class RemoteFeedLoader {
+public final class RemoteFeedLoader {
   private let client: HTTPClient
   private let url: URL
+  
+  public enum Error: Swift.Error {
+    case connectivity
+  }
   
   public init(client: HTTPClient, url: URL) {
     self.client = client
     self.url = url
   }
-  public func load() {
-    client.get(url: self.url)
+  
+  public func load(completion: @escaping (Error) -> Void = { _ in }) {
+    client.get(url: self.url) { error in
+      completion(.connectivity)
+    }
   }
 }
--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -34,6 +34,18 @@ class RemoteFeedLoaderTests: XCTestCase {
     XCTAssertEqual(client.requestedUrls, [url, url])
   }
   
+  func test_load_deliversErrorOnClientError() {
+    let (sut, client) = makeSUT()
+    
+    client.error = NSError(domain: "test", code: 0)
+    var capturedError: RemoteFeedLoader.Error?
+    sut.load() { error in
+      capturedError = error
+    }
+    
+    XCTAssertEqual(capturedError, .connectivity)
+  }
+  
   // MARK: - Helpers
   
   private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
@@ -44,8 +56,12 @@ class RemoteFeedLoaderTests: XCTestCase {
   
   private class HTTPClientSpy: HTTPClient {
     var requestedUrls = [URL]()
+    var error: Error?
     
-    func get(url: URL) {
+    func get(url: URL, completion: @escaping (Error) -> Void) {
+      if let error = self.error {
+        completion(error)
+      }
       requestedUrls.append(url)
     }
   }
