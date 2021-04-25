8a66dd6cc5b3f9bbef13df8598d3f09105de2bf9
Dima Tsurkan <d-link13@mail.ru>
2021-04-25 21:06:02 +0300

    removed magic numbers

--- EssentialFeed/EssentialFeed/Feed API /RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API /RemoteFeedLoader.swift
@@ -68,8 +68,10 @@ private class FeedItemsMapper {
     }
   }
   
+  static let OK_200 = 200
+  
   static func map(_ response: HTTPURLResponse, data: Data) throws -> [FeedItem] {
-    guard response.statusCode == 200 else {
+    guard response.statusCode == OK_200 else {
       throw RemoteFeedLoader.Error.invalidData
     }
     let root = try JSONDecoder().decode(Root.self, from: data)
