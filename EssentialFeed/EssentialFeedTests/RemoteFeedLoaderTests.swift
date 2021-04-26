729bae4e5dc3eb174e59ad4d862b9bf77beac6f8
Dima Tsurkan <d-link13@mail.ru>
2021-04-26 19:44:55 +0300

    added teardown block for tracking memory leaks

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -95,10 +95,20 @@ class RemoteFeedLoaderTests: XCTestCase {
   
   // MARK: - Helpers
   
-  private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+  private func makeSUT(url: URL = URL(string: "https://a-given-url.com")!, file: StaticString = #filePath, line: UInt = #line) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
     let client = HTTPClientSpy()
     let sut = RemoteFeedLoader(client: client, url: url)
+    
+    trackForMemoryLeaks(client, file: file, line: line)
+    trackForMemoryLeaks(sut, file: file, line: line)
+    
     return (sut: sut, client: client)
+  }
+  
+  private func trackForMemoryLeaks(_ instance: AnyObject?, file: StaticString = #filePath, line: UInt = #line) {
+    addTeardownBlock { [weak instance] in
+      XCTAssertNil(instance, "Potential memory leak!", file: file, line: line)
+    }
   }
   
   private func expect(_ sut: RemoteFeedLoader, delivers result: RemoteFeedLoader.Result, when action: () -> Void, file: StaticString = #filePath, line: UInt = #line) {
