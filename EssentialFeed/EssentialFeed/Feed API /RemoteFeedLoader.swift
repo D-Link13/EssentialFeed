648e89ff8a49219f04e9d77e9bd61d8f25b4d343
Dima Tsurkan <d-link13@mail.ru>
2021-04-27 22:29:22 +0300

    - extended FeedLoader interface with generic types - conformed RemoteFeedLoader to the FeedLoader protocol

--- EssentialFeed/EssentialFeed/Feed API /RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API /RemoteFeedLoader.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public final class RemoteFeedLoader {
+public final class RemoteFeedLoader: FeedLoader {
   private let client: HTTPClient
   private let url: URL
   
@@ -16,17 +16,14 @@ public final class RemoteFeedLoader {
     case invalidData
   }
   
-  public enum Result: Equatable {
-    case success([FeedItem])
-    case failure(Error)
-  }
+  public typealias Result = LoadFeedResult<Error>
   
   public init(client: HTTPClient, url: URL) {
     self.client = client
     self.url = url
   }
   
-  public func load(completion: @escaping (Result) -> Void) {
+  public func load(completion: @escaping (LoadFeedResult<Error>) -> Void) {
     client.get(url: self.url) { [weak self] result in
       guard self != nil else { return }
       switch result {
