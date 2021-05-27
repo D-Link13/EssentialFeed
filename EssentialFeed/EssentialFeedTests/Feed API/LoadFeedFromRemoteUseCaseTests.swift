2eae2de7a8a34c41e51fb988c696bc7308749e3b
Dima Tsurkan <d-link13@mail.ru>
2021-05-27 20:52:39 +0300

    renamed FeedLoader tests in according to a name of a use case

--- EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/LoadFeedFromRemoteUseCaseTests.swift
@@ -1,5 +1,5 @@
 //
-//  RemoteFeedLoaderTests.swift
+//  LoadFeedFromRemoteUseCaseTests.swift
 //  EssentialFeedTests
 //
 //  Created by Dmitry Tsurkan on 20.04.2021.
@@ -8,7 +8,7 @@
 import XCTest
 import EssentialFeed
 
-class RemoteFeedLoaderTests: XCTestCase {
+class LoadFeedFromRemoteUseCaseTests: XCTestCase {
   func test_init_doesNotRequestDataFromUrl() {
     let (_, client) = makeSUT()
     
