f4912d5ae6e9a2bb60bcabdcd517d5238bcc27a9
Dima Tsurkan <d-link13@mail.ru>
2021-05-28 21:57:55 +0300

    added test save calls deletion request, added helper method to our tests

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import EssentialFeed
 
 class LocalFeedLoader {
+  private var store: FeedStore
+  
   init(store: FeedStore) {
-    
+    self.store = store
+  }
+  
+  func save(_ items: [FeedItem]) {
+    store.deleteCachedFeed()
   }
 }
 
 class FeedStore {
   var deleteCachedFeedCallCount = 0
+  
+  func deleteCachedFeed() {
+    deleteCachedFeedCallCount += 1
+  }
 }
 
 class CacheFeedUseCaseTests: XCTestCase {
@@ -24,6 +35,22 @@ class CacheFeedUseCaseTests: XCTestCase {
     _ = LocalFeedLoader(store: store)
     
     XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+  }
+  
+  func test_save_requestsCacheDeletionOnce() {
+    let store = FeedStore()
+    let sut = LocalFeedLoader(store: store)
+    let items = [uniqueItem(), uniqueItem()]
+    
+    sut.save(items)
+    
+    XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+  }
+  
+  // MARK: - Helpers
+  
+  func uniqueItem() -> FeedItem {
+    return FeedItem(id: UUID(), description: "any", location: "any", imageUrl: anyURL())
   }
   
 }
