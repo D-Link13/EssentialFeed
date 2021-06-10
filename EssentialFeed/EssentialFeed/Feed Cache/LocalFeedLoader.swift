c16c7d20ad449c57c32127516ae88224b4680407
Dima Tsurkan <d-link13@mail.ru>
2021-06-10 16:22:04 +0300

    FeedLoader conformance

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -55,7 +55,7 @@ extension LocalFeedLoader {
   
 }
   
-extension LocalFeedLoader {
+extension LocalFeedLoader: FeedLoader {
   public func load(_ completion: @escaping (LoadResult) -> Void) {
     store.retrieve { [weak self] result in
       guard let self = self else { return }
