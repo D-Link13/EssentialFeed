42923550de090445778a910114ee70741c84d691
Dima Tsurkan <d-link13@mail.ru>
2021-06-10 16:25:49 +0300

    moved typealiases to the extensions respectively

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -9,8 +9,6 @@ import Foundation
 
 public final class LocalFeedLoader {
   public typealias CurrentDateProvider = () -> Date
-  public typealias SaveResult = Error?
-  public typealias LoadResult = LoadFeedResult
   
   private let currentDate: CurrentDateProvider
   private let store: FeedStore
@@ -35,6 +33,8 @@ public final class LocalFeedLoader {
 }
   
 extension LocalFeedLoader {
+  public typealias SaveResult = Error?
+  
   public func save(_ feed: [FeedImage], completion: @escaping (SaveResult) -> Void) {
     store.deleteCachedFeed() { [weak self] error in
       guard let self = self else { return }
@@ -56,6 +56,8 @@ extension LocalFeedLoader {
 }
   
 extension LocalFeedLoader: FeedLoader {
+  public typealias LoadResult = LoadFeedResult
+  
   public func load(completion: @escaping (LoadResult) -> Void) {
     store.retrieve { [weak self] result in
       guard let self = self else { return }
