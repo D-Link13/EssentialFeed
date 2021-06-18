2815af835c3cb7e57f90252087e89309f2a09b60
Dima Tsurkan <d-link13@mail.ru>
2021-06-18 20:13:57 +0300

    extracted store url with the initialiser injection

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -54,7 +54,11 @@ class CodableFeedStore {
     }
   }
   
-  private let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+  private let storeURL: URL
+  
+  init(storeURL: URL) {
+    self.storeURL = storeURL
+  }
   
   func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertCompletion) {
     let codableFeed = feed.map { CodableFeedImage(local: $0) }
@@ -145,7 +149,8 @@ class CodableFeedStoreTests: XCTestCase {
  
   // MARK: - Helpers
   func makeSUT(file:StaticString = #filePath, line:UInt = #line) -> CodableFeedStore {
-    let sut = CodableFeedStore()
+    let storeURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image-feed.store")
+    let sut = CodableFeedStore(storeURL: storeURL)
     trackForMemoryLeaks(sut, file: file, line: line)
     return sut
   }
