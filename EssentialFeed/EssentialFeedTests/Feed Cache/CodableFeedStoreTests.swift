c146ee5c258ab31610f05d82447e8d12d9dd9ff5
Dima Tsurkan <d-link13@mail.ru>
2021-06-21 20:01:42 +0300

    Retrieve has no side effects on invalid data (e. g. retrieve twice returns failure twice)

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -140,6 +140,15 @@ class CodableFeedStoreTests: XCTestCase {
     
     expect(sut, toRetrieve: .failure(anyNSError()))
   }
+  
+  func test_retrieve_hasNoSideEffectsOnInvalidData() {
+    let storeURL = testSpecificStoreURL()
+    let sut = makeSUT(storeURL: storeURL)
+    
+    try! "invalid data".write(to: storeURL, atomically: false, encoding: .utf8)
+    
+    expect(sut, toRetrieveTwice: .failure(anyNSError()))
+  }
  
   // MARK: - Helpers
   
